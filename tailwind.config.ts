e86c59379f2bcbd7e2c5c4704a326360f23ef585
torresdesenv <renato.torres.campos@hotmail.com>
2026-03-09 23:02:24 -0300

    Fix: ajustando caminhos do Tailwind e Layout para pasta src

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,13 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // Garante que ele olhe dentro de src/app
   ],
   theme: {
     extend: {
       colors: {
         easyBlue: "#0047AB",
         easyOrange: "#FF8C00",
-        slate: {
-          900: "#0f172a",
-        }
       },
     },
   },
