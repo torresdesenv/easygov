ce35bb390b0e729e49be6be70eb3fd6fdf30727c
torresdesenv <renato.torres.campos@hotmail.com>
2026-03-09 23:28:23 -0300

    Fix: migração completa para Tailwind v4 e PostCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Varre todos os arquivos dentro de src
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
