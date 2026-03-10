1f886d331e7c3c17249e407ca341ebcfd6c77c45
torresdesenv <renato.torres.campos@hotmail.com>
2026-03-09 23:21:36 -0300

    Fix: instalando autoprefixer e corrigindo PostCSS para Vercel

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,6 @@
 export default {
   plugins: {
-    "@tailwindcss/postcss": {}, // Se estiver usando Tailwind 4
-    // ou use 'tailwindcss': {}, se estiver na versão 3
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Esta linha cobre TUDO dentro de src
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Varre todos os arquivos dentro de src
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
